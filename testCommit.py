d6edabe465d4f964bf6232f2dce5fd7845778ce7
Chetanya Chopra <chetanya221997chopra@gmail.com>
2017-03-10 00:10:21 +0530

    basic syntax practice

---
+++ testCommit.py
@@ -0,0 +1,11 @@
+def number_print():
+	for i in range(1,2):
+		for x in range(1,10):
+			print (x)
+array=[1,"chetanya","python",'g']
+
+def array_print():
+	for arr_item in array:
+		print (arr_item)
+
+array_print();
