b251f1ee6de226a0960cb40e42cb3cc17f153344
chetanyachopra <chetanya221997chopra@gmail.com>
2017-03-11 23:49:20 +0530

    basic syntax revised

--- Basic/classesbasics.py
+++ Basic/classesbasics.py
@@ -1,6 +1,31 @@
-class myclass():
-	a=5
-	def myfn():
-		f=5;
+class student:
+	'''def __init__(self, roll, name): 
+		self.roll=roll
+		self.name=name'''
+	def details(self):
+		#print("student:	"+"rollno=",self.roll,"	name= "+self.name)
+		print("details of student")
+class subjectsOpt(student):
+	# def __init__(self):
+	# 	student.roll=roll
+	# 	student.name=namef
+	def name(self):
+		print("name called")
+'''emp1=student(1510991158,"chetanya")
+emp2=student(1510991156,"chetab")
 
-print(myclass.a)
+emp2.details()
+emp1.details()'''
+def factorial(num):
+	fact=1;
+	while num>0:
+		if num==0:
+			return fact
+		fact=fact*num
+		--num
+
+
+st=subjectsOpt();
+st.details();
+st.name();
+# print(factorial(5))
